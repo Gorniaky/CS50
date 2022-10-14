2497f7b53d7e0cc9cd4d83ec0490505d8a7e831b
Gorniaky <hwss.s2@gmail.com>
2022-10-14 10:31:43 -0300

    pset7 better solution

--- pset7/fiftyville/log.sql
+++ pset7/fiftyville/log.sql
@@ -26,47 +26,7 @@ Eugene | Não sei o nome do ladrão, mas era alguém que reconheci. No início d
 Raymond | Quando o ladrão estava saindo do tribunal, eles ligaram para alguém que conversou com eles por menos de um minuto. Na ligação, ouvi o ladrão dizer que eles planejavam pegar o primeiro voo de Fiftyville amanhã. O ladrão então pediu à pessoa do outro lado do telefone para comprar a passagem aérea.
 */
 
-SELECT people.name, people.phone_number, phone_calls.receiver, airports.city
-FROM people
-JOIN courthouse_security_logs
-ON people.license_plate = courthouse_security_logs.license_plate
-JOIN atm_transactions
-ON people.id = bank_accounts.person_id
-JOIN bank_accounts
-ON bank_accounts.account_number = atm_transactions.account_number
-JOIN phone_calls
-ON phone_calls.caller = people.phone_number
-JOIN passengers
-ON people.passport_number = passengers.passport_number
-JOIN flights
-ON passengers.flight_id = flights.id
-JOIN airports
-ON flights.destination_airport_id = airports.id
-WHERE courthouse_security_logs.month = 7
-AND courthouse_security_logs.day = 28
-AND courthouse_security_logs.hour = 10
-AND courthouse_security_logs.minute > 15
-AND courthouse_security_logs.minute < 25
-AND atm_transactions.month = 7
-AND atm_transactions.day = 28 
-AND atm_transactions.atm_location = "Fifer Street" 
-AND atm_transactions.transaction_type = "withdraw"
-AND phone_calls.month = 7
-AND phone_calls.day = 28
-AND phone_calls.duration < 60;
-
-/*
-Russell | 26013199 | (770) 555-1861 | (725) 555-3243
-Ernest | 49610011 | (367) 555-5533 | (375) 555-8161
-Madison | 76054385 | (286) 555-6063 | (676) 555-6554
-*/
-
-SELECT DISTINCT(people.name), people.phone_number, phone_calls.caller
-FROM people
-JOIN phone_calls
-ON phone_calls.receiver = people.phone_number
-WHERE people.phone_number IN (
-SELECT phone_calls.receiver
+SELECT people.name, people.phone_number, accomplice.name, phone_calls.receiver, destination.city
 FROM people
 JOIN courthouse_security_logs
 ON people.license_plate = courthouse_security_logs.license_plate
@@ -82,6 +42,10 @@ JOIN flights
 ON passengers.flight_id = flights.id
 JOIN airports
 ON flights.origin_airport_id = airports.id
+JOIN airports AS destination
+ON flights.destination_airport_id = destination.id
+JOIN people AS accomplice
+ON phone_calls.receiver = accomplice.phone_number
 WHERE courthouse_security_logs.month = 7
 AND courthouse_security_logs.day = 28
 AND courthouse_security_logs.hour = 10
@@ -94,5 +58,10 @@ AND atm_transactions.transaction_type = "withdraw"
 AND phone_calls.month = 7
 AND phone_calls.day = 28
 AND phone_calls.duration < 60
-AND airports.city = "Fiftyville"
-);
+AND flights.day = 29
+ORDER BY flights.hour
+LIMIT 1;
+
+/*
+Ernest | (367) 555-5533 | Berthold | (375) 555-8161 | London
+*/
